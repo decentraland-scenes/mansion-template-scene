7f12b8e6989499e6440c97def9e596c4055998b0
nearnshaw <earnshaw.nico@gmail.com>
2023-06-20 09:30:38 -0300

    clean up code

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,8 @@
-import { engine, executeTask, Material } from '@dcl/sdk/ecs'
-import { Color4 } from '@dcl/sdk/math'
-
-import { createCube } from './factory'
 
 import { setupUi } from './ui'
 
 
 export function main() {
-	setupUi()
+	//setupUi()
 
 }
--- src/ui.tsx
+++ src/ui.tsx
@@ -1,10 +1,4 @@
-import {
-  engine,
-  Transform,
-} from '@dcl/sdk/ecs'
-import { Color4 } from '@dcl/sdk/math'
 import ReactEcs, { Button, Label, ReactEcsRenderer, UiEntity } from '@dcl/sdk/react-ecs'
-import { createCube } from './factory'
 
 const uiComponent = () => (
  
